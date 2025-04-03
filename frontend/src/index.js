47e51b04dd301e5867382975010fbcb04d24eb26
PedroLucas003 <pedrolucasaraujo201733@gmail.com>
2025-04-02 21:25:48 -0300

    02/04/2025novapagina

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RegisterUser from "./RegisterUser";
-import Login from "./Login";
-import Prontuario from "./Prontuario";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import RegisterUser from './RegisterUser';
+import Login from './Login';
+import Prontuario from './Prontuario';
+import './index.css'; // Adicione este import para estilos globais
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegisterUser />} />
         <Route path="/prontuario" element={<Prontuario />} />
